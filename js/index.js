25aa2b0da4ecf051800eaa854d797dc83808b5c2
mrdrewkeller <drew.s.keller@gmail.com>
2017-04-15 13:02:22 -0400

    adds counter

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 var width = 840,
   height = 620;
 
-var accidents = "http://174.138.68.14:3000/accidents?amateurbuilt=eq.Yes&publicationdate=gt.2016-04-04 00:00:00";
-request = new XMLHttpRequest();
-request.open('GET', accidents, true);
-request.onload = function () {
-  accidentLocations = JSON.parse(request.responseText);
-};
-request.send();
+var accidents = "http://174.138.68.14:3000/accidents?amateurbuilt=eq.Yes&publicationdate=gt.2016-04-04";
+
 
 
 // Add #container div, with #svg-container nested
@@ -59,19 +54,19 @@ d3.json("https://raw.githubusercontent.com/moigithub/d3maplayout/master/world-50
 
   d3.json(accidents, function (error, data) {
     if (error) return console.log("error fetching accident JSON");
+document.getElementById("foldable").innerHTML = data.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " incidents";
     // Bind data to circles
     var points = svg.selectAll("g").data(data).enter().append("g");
 
 
     // Render the circles
-    points.append("circle").attr("r", .3).attr("cx", function (d) {
+    points.append("circle").attr("r", .7).attr("cx", function (d) {
         var coords = [d.longitude, d.latitude];
         if (coords) {
           return projection(coords)[0];
         }
       }).attr("cy", function (d) {
         var coords = [d.longitude, d.latitude];
-        console.log(projection(coords));
         if (coords) {
           return projection(coords)[1];
         }
@@ -91,7 +86,7 @@ d3.json("https://raw.githubusercontent.com/moigithub/d3maplayout/master/world-50
         //var year = yearFormat(new Date(data.year));
         tooltip.transition().duration(200).style("opacity", 1);
         // Add text to .tooltip
-        tooltip.html("<h2 class='title'>" + data.location + "</h2>" + "<h3>Fell: " + data.country + "</h3>" + "<strong>Mass</strong>: " + (data.mass / 1000).toFixed(2) + " KG<br />");
+        tooltip.html("<h2 class='title'>" + data.location + "</h2>" + "<h3>" + data.country + "</h3>"  + data.accidentnumber + "<br />");
         // Check if tooltip is too close to right side of page, remove by toolwidth length if it is
         if (d3.event.pageX + 260 > window.innerWidth) {
           tooltip.style("left", d3.event.pageX - tooltip.style("width").replace(/[px]/g, '') + "px");
