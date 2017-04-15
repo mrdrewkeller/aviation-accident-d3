0a4a8d5a92f8f9e4a4b4d00cc41bcefb38695dc2
mrdrewkeller <drew.s.keller@gmail.com>
2017-04-15 14:00:26 -0400

    functionalizes map and plotting

--- js/index.js
+++ js/index.js
@@ -1,60 +1,89 @@
-var width = 840,
-  height = 620;
-
+var width = 840;
+var height = 620;
 var accidents = "http://174.138.68.14:3000/accidents?amateurbuilt=eq.Yes&publicationdate=gt.2016-04-04";
 
+window.onload = function () {
+  drawMap();
+  plotPoints();
+}
 
+function drawMap() {
+  // Add #container div, with #svg-container nested
+  // This allows the svg to use preserveAspectRatio
+  // and viewBox to make the svg responsive
+  svg =
+    d3.select("span")
+    .append("div")
+    .attr("id", "container")
+    .append("div")
+    .attr("id", "svg-container")
+    .append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 " + width + " " + height)
+    .attr("id", "svg-content-responsive")
+    .call(d3.behavior.zoom().on("zoom", function () {
+      svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+    }))
+    .append("g");
 
-// Add #container div, with #svg-container nested
-// This allows the svg to use preserveAspectRatio
-// and viewBox to make the svg responsive
-var svg = 
-  d3.select("span")
-  .append("div")
-  .attr("id", "container")
-  .append("div")
-  .attr("id", "svg-container")
-  .append("svg")
-  .attr("preserveAspectRatio", "xMinYMin meet")
-  .attr("viewBox", "0 0 " + width + " " + height)
-  .attr("id", "svg-content-responsive")
-  .call(d3.behavior.zoom().on("zoom", function () {
-    svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
-  }))
-  .append("g");
+  // Define the tooltip
+  tooltip =
+    d3.select("span")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-// Define the tooltip
-var tooltip = 
-  d3.select("span")
-  .append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0);
+  // Define projection
+  projection = d3.geo.mercator().translate([width / 2, height / 2.6]);
+  // Define path generator
+  path = d3.geo.path().projection(projection);
 
-// Define projection
-var projection = d3.geo.mercator().translate([width / 2, height / 2.6]);
-// Define path generator
-var path = d3.geo.path().projection(projection);
+  // Get data and render map with callback
+  d3.json("https://raw.githubusercontent.com/moigithub/d3maplayout/master/world-50m.json", function (error, data) {
+    if (error) return console.log("error retrieving json");
 
-// Get data and render map with callback
-d3.json("https://raw.githubusercontent.com/moigithub/d3maplayout/master/world-50m.json", function (error, data) {
-  if (error) return console.log("error retrieving json");
+    // Get feature collection of world map
+    subunits = topojson.feature(data, data.objects.countries);
 
-  // Get feature collection of world map
-  var subunits = topojson.feature(data, data.objects.countries);
+    // Render path
+    svg.append("path").datum(subunits).attr("d", path);
 
-  // Render path
-  svg.append("path").datum(subunits).attr("d", path);
+    // Create .subunit paths of each country
+    svg.selectAll(".subunit")
+      .data(topojson.feature(data, data.objects.countries).features)
+      .enter()
+      .append("path").attr("class", "subunit")
+      .attr("d", path);
 
-  // Create .subunit paths of each country
-  svg.selectAll(".subunit")
-  .data(topojson.feature(data, data.objects.countries).features)
-  .enter()
-  .append("path").attr("class", "subunit")
-  .attr("d", path);
+    zoom =
+      d3.behavior.zoom()
+      .translate(projection.translate())
+      .scale(projection.scale())
+      .scaleExtent([height, 8 * height])
+      .on("zoom", zoomed);
 
+    function clicked(d) {
+      var centroid = path.centroid(d),
+        translate = projection.translate();
+
+      projection.translate([translate[0] - centroid[0] + width / 2, translate[1] - centroid[1] + height / 2]);
+
+      zoom.translate(projection.translate());
+
+      g.selectAll("path").transition().duration(700);
+    }
+
+    function zoomed() {
+      projection.translate(d3.event.translate).scale(d3.event.scale);
+      g.selectAll("path");
+    }
+  });
+}
+
+function plotPoints() {
   d3.json(accidents, function (error, data) {
     if (error) return console.log("error fetching accident JSON");
-document.getElementById("foldable").innerHTML = data.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " incidents";
+    document.getElementById("foldable").innerHTML = data.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " incidents";
     // Bind data to circles
     var points = svg.selectAll("g").data(data).enter().append("g");
 
@@ -86,7 +115,7 @@ document.getElementById("foldable").innerHTML = data.length.toString().replace(/
         //var year = yearFormat(new Date(data.year));
         tooltip.transition().duration(200).style("opacity", 1);
         // Add text to .tooltip
-        tooltip.html("<h2 class='title'>" + data.location + "</h2>" + "<h3>" + data.country + "</h3>"  + data.accidentnumber + "<br />");
+        tooltip.html("<h2 class='title'>" + data.location + "</h2>" + "<h3>" + data.country + "</h3>" + data.accidentnumber + "<br />");
         // Check if tooltip is too close to right side of page, remove by toolwidth length if it is
         if (d3.event.pageX + 260 > window.innerWidth) {
           tooltip.style("left", d3.event.pageX - tooltip.style("width").replace(/[px]/g, '') + "px");
@@ -106,27 +135,4 @@ document.getElementById("foldable").innerHTML = data.length.toString().replace(/
         tooltip.transition().duration(200).style("opacity", 0);
       });
   });
-
-  var zoom =
-    d3.behavior.zoom()
-    .translate(projection.translate())
-    .scale(projection.scale())
-    .scaleExtent([height, 8 * height])
-    .on("zoom", zoomed);
-
-  function clicked(d) {
-    var centroid = path.centroid(d),
-      translate = projection.translate();
-
-    projection.translate([translate[0] - centroid[0] + width / 2, translate[1] - centroid[1] + height / 2]);
-
-    zoom.translate(projection.translate());
-
-    g.selectAll("path").transition().duration(700);
-  }
-
-  function zoomed() {
-    projection.translate(d3.event.translate).scale(d3.event.scale);
-    g.selectAll("path");
-  }
-});
+}
